927b80619225349f49068b9a4dc06c5fc2855002
Lei Chun <136141200+YanxiangDu2023@users.noreply.github.com>
2024-11-05 23:04:26 +0100

    Delete Desktop/coding/Reggie-take-out_/coding/day03/reggie_take_out directory

--- Desktop/coding/Reggie-take-out_/coding/day03/reggie_take_out/src/main/java/com/itheima/reggie/service/SetmealService.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.reggie.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.itheima.reggie.entity.Setmeal;
-
-public interface SetmealService extends IService<Setmeal>{
-
-}
-
